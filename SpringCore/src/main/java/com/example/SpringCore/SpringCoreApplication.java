425cb7e139c4d15fee6f4a92389e4dba6f27ea9a
vinayjadaun11112003 <vinayjadaun11112003@gmail.com>
2025-02-24 07:35:26 +0530

    Initializing Spring core repo

---
+++ SpringCore/src/main/java/com/example/SpringCore/SpringCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreApplication {
+    //UC01 : Application Created with spring initiallizer
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreApplication.class, args);
+	}
+
+}
