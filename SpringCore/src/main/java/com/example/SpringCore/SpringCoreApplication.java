b8fcf640bc430b49b1c52f09dba23e9a99a14d41
vinayjadaun11112003 <vinayjadaun11112003@gmail.com>
2025-02-24 07:39:01 +0530

    [Vinay Jadaun] - Add. Application spring core created with spring initializer

--- SpringCore/src/main/java/com/example/SpringCore/SpringCoreApplication.java
+++ SpringCore/src/main/java/com/example/SpringCore/SpringCoreApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringCoreApplication {
-    //UC01 : Application Created with spring initiallizer
+    //UC01 : Application Created with spring initiallizer.
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCoreApplication.class, args);
 	}
